87fe143f08ec8a39714b69d7eea26f411ad91eb7
chansenhyggedev <chansen.hyggedev@gmail.com>
2023-02-10 15:59:12 -0800

    update

--- src/components/Figures.jsx
+++ src/components/Figures.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import CountUp from 'react-countup';
 
 const Figures = () => {
+
+    let data = [
+        {
+            figure: '49',
+            unit: 'k',
+            text: 'users'
+        },
+        {
+            figure: '2.2',
+            unit: 'k',
+            text: 'enterprise subscribers'
+        },
+        {
+            figure: '17',
+            unit: '',
+            text: 'countries'
+        },
+        {
+            figure: '96',
+            unit: '%',
+            text: 'success rate'
+        },
+    ]
     return (
-        <div className='w-full py-24'>
+        <div className='w-full pt-24 pb-14'>
             <div className="mxw-sm flex items-center gap-8 flex-wrap justify-evenly">
-                <div className='text-center'>
-                    <h2 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold md:font-black text-gray-100'>49k+</h2>
-                    <p className='text-gray-100 font-light text-lg md:text-xl'>users</p>
-                </div>
-                <div className='text-center'>
-                    <h2 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold md:font-black text-gray-100'>2.2k+</h2>
-                    <p className='text-gray-100 font-light text-lg md:text-xl'>subscribers</p>
-                </div>
-                <div className='text-center'>
-                    <h2 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold md:font-black text-gray-100'>198k+</h2>
-                    <p className='text-gray-100 font-light text-lg md:text-xl'>users</p>
-                </div>
-                <div className='text-center'>
-                    <h2 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold md:font-black text-gray-100'>76%</h2>
-                    <p className='text-gray-100 font-light text-lg md:text-xl'>users</p>
-                </div>
+                {
+                    data?.map(i => (
+                        <div className='text-center space-y-3'>
+                            <h2 className='text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold md:font-black text-gray-100'>
+                                <CountUp start={0} end={i?.figure} />
+                                {i?.unit}
+                            </h2>
+                            <p className='text-gray-50 font-thin text-lg md:text-xl'>{i.text}</p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = () => {
             <img className='h-full object-cover sm:w-1/2 grayscale' src="https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
             <img className='absolute -bottom-14 left-1/2 -translate-x-1/2 z-10 scale-75 md:scale-90' src="/circle-asset.png" alt="" />
             <div className='absolute inset-0 h-full w-full bg-gradient-to-r from-black via-black/80'>
-                <div className="mxw pt-28 flex items-center">
+                <div className="mxw pt-16 sm:pt-24 lg:pt-28 flex items-center">
                     <div className=" text-gray-100 space-y-6">
-                        <h2 className='text-[8vw] sm:text-5xl lg:text-7xl xl:text-[6vw] font-black uppercase'>Lorem ipsum dolor sit.</h2>
+                        <h2 className='text-[7vw] sm:text-5xl lg:text-7xl xl:text-[5vw] font-black uppercase'>Team Management meets modern teams</h2>
                         <p className='max-w-xl text-xl md:text-2xl font-thin'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vel accusantium expedita?</p>
                         <div className='inline-flex items-center gap-5 group cursor-pointer'>
                             <button type="button" className='bg-none outline-none border-none text-xl md:text-2xl lg:text-3xl'>Get Started</button>
